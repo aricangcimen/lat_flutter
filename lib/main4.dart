b0f7421ea9d8b1cfe962f8e9493040f3a0d7006e
aricangcimen <antosan1234567@gmail.com>
2019-05-21 12:22:47 +0700

    tambah 4 item

--- lib/main4.dart
+++ lib/main4.dart
@@ -36,7 +36,28 @@ class Halsatu extends StatelessWidget {
           ],
         )
         
+                ),
+            
+           new Card(
+        child:new Column(
+          children: <Widget>[
+        new Icon(Icons.call_missed , size : 50.0 ,  color : Colors.white , ),
+        new Text("Home",style: new TextStyle(fontSize: 20.0),)
+          ],
+        )
+        
+                ) ,
+            
+            new Card(
+        child:new Column(
+          children: <Widget>[
+        new Icon(Icons.call_missed , size : 50.0 ,  color : Colors.white , ),
+        new Text("Home",style: new TextStyle(fontSize: 20.0),)
+          ],
+        )
+        
                 )
+            
             ],
 
 
