2f1eb04539418b710e8e63f839844eebf3ba3178
aricangcimen <antosan1234567@gmail.com>
2019-05-21 09:45:24 +0700

    masukan center

--- lib/main1.dart
+++ lib/main1.dart
@@ -11,14 +11,19 @@ class Halamansatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Container(
+
+      body: new Center(
+
+        child:new Container(
         color:Colors.blue[900],
         width: 200.0,
         height: 100.0,
         child: new Center(
           child: new Text("Idcorner Tutorial",style: new TextStyle(color: Colors.white,fontFamily: "Serif",fontSize: 20.0),),
         ),
-      ),
+      ), ,
+      )
+      
       
     );
   }
