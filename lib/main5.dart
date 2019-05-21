eb3976f0b17d6fe6847aed4585eb092bcc934012
aricangcimen <antosan1234567@gmail.com>
2019-05-21 13:28:25 +0700

    Navigasi

--- lib/main5.dart
+++ lib/main5.dart
@@ -4,6 +4,12 @@ void main(){
 runApp(MaterialApp(
 home: new Haldua(),
 title: "Navigasi",
+routes: <String ,WidgetBuilder>{
+'/Halsatu' : (BuildContext context) => new Halsatu(),
+'/Haldua' : (BuildContext context) => new Haldua(),
+
+
+},
 ));
 }
 
@@ -15,14 +21,29 @@ class Haldua extends StatelessWidget {
       body: new Center(
 child: new IconButton(
 icon:  new Icon(Icons.change_history, size : 50.0 , ),
-onPressed: ,
+onPressed: null ,
+),
+      ),
+      
+    );
+  }
+}
+
+class Halsatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title:  new Text("Speaker"),),
+      body: new Center(
+child: new IconButton(
+  icon: new Icon(Icons.chat, size: 50.0 , color : Colors.blueGrey,)
 
 
 ),
 
 
+      )
 
-      ),
       
     );
   }
