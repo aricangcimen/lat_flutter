0e647cd1f2bfc4c4fa5231add648699a292a5aa8
aricangcimen <antosan1234567@gmail.com>
2019-05-22 02:28:48 +0700

    sudah selesai

--- lib/main7.dart
+++ lib/main7.dart
@@ -12,7 +12,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
+void _snackbar(String str){
+if(str.isEmpty ) return ;
+_scaffoldState.currentState.showSnackBar(new SnackBar(
+content: new Text(str , style : new TextStyle(fontSize: 20.0), ),
+duration: new Duration(seconds: 3),
+));
+
+
+
+}
+
+
+
   
   
 void _alretdialog(String str){
@@ -42,10 +54,15 @@ showDialog(context: context , child: alertDialog );
  String teks = "";
  TextEditingController controllerInput = new TextEditingController();
   TextEditingController controllerAllert = new TextEditingController();
+TextEditingController controllerSnackbar = new TextEditingController();
+
+final GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: _scaffoldState,
       appBar: new AppBar(title: new Text( "Input Text , Alret dialog & snackbar"),
       backgroundColor: Colors.purple,),
       body: new Container(
@@ -72,11 +89,23 @@ decoration: new InputDecoration(
 ),
 onSubmitted: (String str){
 _alretdialog(str);
+controllerAllert.text="";
 
 
 },
             ),
+ new TextField(
+              controller: controllerSnackbar,
+decoration: new InputDecoration(
+  hintText: "tulislah untuk Snackbar....",
+),
+onSubmitted: (String str){
+_snackbar(str);
+controllerSnackbar.text="";
 
+
+},
+            ),
          
 
 
